106d1e226bb9138aaf8e68417926bb0a60164f63
Paweł Gasowski <pawelix94@gmail.com>
2020-04-26 14:19:08 +0200

    #3 Add CORS, axios and ant.design

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -33,13 +39,15 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }            
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import { PageHeader, List } from 'antd';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-     <ul>
+class App extends Component {
+  state = {
+    books: []
+  }
 
-     </ul>
-    </div>
-  );
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/books').then(response => {
+      this.setState({
+        books: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <PageHeader
+          className="site-page-header"
+          title="task"
+        />
+        <List>
+          {this.state.books.map((books: any) => (
+            <List.Item key={books.id}>{books.title}</List.Item>                     
+          ))}
+        </List>
+      </div>
+    );
+  }
 }
+
 
 export default App;
