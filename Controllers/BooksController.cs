19ca1a72b68986540f48db3a3801370c2c31f2ad
Paweł Gasowski <pawelix94@gmail.com>
2020-04-26 12:21:02 +0200

    #3 API Controller and React

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,10 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BooksController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public BooksController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> Get()
+        {
+            var books = await _context.Books.ToListAsync();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> Get(int id)
+        {
+            var result = await _context.Books.FindAsync(id);
+            if (result == null)
+                return NotFound($"nie znaleziono ksiazki o id: {id}");
+
+            return Ok(result);
+        }
     }
 }
